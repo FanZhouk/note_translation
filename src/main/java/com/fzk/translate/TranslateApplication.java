aef335879b3dea238593ed60deb46ebbd96b0fac
xnfanzhoukai <fanzhoukai@imdada.cn>
2019-01-17 16:15:25 +0800

    添加打包代码

--- src/main/java/com/fzk/translate/TranslateApplication.java
+++ src/main/java/com/fzk/translate/TranslateApplication.java
@@ -3,17 +3,24 @@ package com.fzk.translate;
 import com.fzk.translate.strategy.JavaPreprocessStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 // 扫描translate包下的所有组件
 @ComponentScan(value = {"com.fzk.translate"})
-public class TranslateApplication {
+public class TranslateApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TranslateApplication.class, args);
     }
 
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
 
